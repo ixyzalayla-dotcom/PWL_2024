02b73a0711257d3a3ca0bf4cb27e4350f8f3ee1b
ixyzalyla-dotcom <ixyzalayla@gamil.com>
2026-04-29 11:36:55 +0700

    modifikasi tampilan awal

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/hello', function () {
+    return "Hello";
+});
+Route::get('/world', function () {
+    return "World";
+});
+
+// 8. Route dengan satu parameter
+Route::get('/user/{name}', function ($name) {
+    return "Nama saya: " . $name;
+});
+
+// 11. Route dengan multiple parameter
+Route::get('/posts/{postId}/comments/{commentId}', function ($postId, $commentId) {
+    return "Post ke-" . $postId . " Comment ke-" . $commentId;
+});
+
+// 13. Route articles dengan parameter id
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID " . $id;
+});
